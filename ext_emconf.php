85d5868246ff65c5d5fb73ca97d6b9769f605572
Manuel Schnabel <mschnabel1992@gmail.com>
2023-02-17 13:21:42 +0100

    [RELEASE] Set version to 0.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'PassionWeb Manuel Schnabel',
     'state' => 'beta',
     'clearCacheOnLoad' => true,
-    'version' => '0.1.0',
+    'version' => '0.1.1',
     'constraints' => [
         'depends' => ['typo3' => '11.5.0-12.2.0'],
         'conflicts' => [],
