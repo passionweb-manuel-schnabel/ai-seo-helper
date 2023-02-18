6842b0f0f494f2e685f84e6ddb6a17dff420cef9
Manuel Schnabel <mschnabel1992@gmail.com>
2023-02-18 23:42:31 +0100

    [BUGFIX] Prevent pointer-events by clicking directly on the icons of the field controls

---
+++ ext_tables.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+defined('TYPO3') or die();
+
+// Register as a skin
+$GLOBALS['TBE_STYLES']['skins']['ai_seo_helper']['stylesheetDirectories']['css'] = 'EXT:ai_seo_helper/Resources/Public/Css/';
