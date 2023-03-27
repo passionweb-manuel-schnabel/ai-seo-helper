24e9c117be03c9bed4708591c3e872a9bd5f8878
Manuel Schnabel <mschnabel1992@gmail.com>
2023-03-27 22:03:38 +0200

    [TASK] Add CustomLanguageFactory

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -5,6 +5,10 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Log\LogManager;
+use Psr\Log\LoggerInterface;
+use Passionweb\AiSeoHelper\Service\ContentService;
+use Passionweb\AiSeoHelper\Factory\CustomLanguageFactory;
+use Passionweb\AiSeoHelper\Controller\Ajax\AiController;
 
 return static function (ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void {
     $services = $containerConfigurator->services();
@@ -20,25 +24,25 @@ return static function (ContainerConfigurator $containerConfigurator, ContainerB
 
     $services->set('ExtConf.aiSeoHelper', 'array')
         ->factory([service(ExtensionConfiguration::class), 'get'])
-        ->args(
-            [
-                'ai_seo_helper'
-            ]
-        );
+        ->args([
+            'ai_seo_helper'
+        ]);
 
-    $containerBuilder->register('Logger', \Psr\Log\LoggerInterface::class);
+    $services->set('CustomLanguageArray', 'array')
+        ->factory([service(CustomLanguageFactory::class), 'getCustomLanguages']);
+
+    $containerBuilder->register('Logger', LoggerInterface::class);
     $services->set('PsrLogInterface', 'Logger')
-        ->factory(
-            [
-                service(LogManager::class), 'getLogger'
-            ]
-        );
+        ->factory([
+            service(LogManager::class), 'getLogger'
+        ]);
 
-    $services->set(\Passionweb\AiSeoHelper\Service\ContentService::class)
-        ->arg('$extConf', service('ExtConf.aiSeoHelper'))
-        ->public();
+    $services->set(ContentService::class)
+        ->arg('$languages', service('CustomLanguageArray'))
+        ->arg('$extConf', service('ExtConf.aiSeoHelper'));
 
-    $services->set(\Passionweb\AiSeoHelper\Controller\Ajax\AiController::class)
+    $services->set(AiController::class)
+        ->arg('$contentService', service(ContentService::class))
         ->arg('$logger', service('PsrLogInterface'))
         ->public();
 };
