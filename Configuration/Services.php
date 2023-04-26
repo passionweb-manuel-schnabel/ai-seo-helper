a2e0a62a06486dcad4154eca6df39f3cc7296802
Manuel Schnabel <mschnabel1992@gmail.com>
2023-04-26 15:41:56 +0200

    [BUGFIX] Change service function to ReferenceConfigurator instance (for v10 compatibility)

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -2,14 +2,13 @@
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ReferenceConfigurator;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Log\LogManager;
 use Psr\Log\LoggerInterface;
 use Passionweb\AiSeoHelper\Service\ContentService;
 use Passionweb\AiSeoHelper\Factory\CustomLanguageFactory;
 use Passionweb\AiSeoHelper\Controller\Ajax\AiController;
-
-use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void {
     $services = $containerConfigurator->services();
@@ -24,26 +23,26 @@ return static function (ContainerConfigurator $containerConfigurator, ContainerB
         ]);
 
     $services->set('ExtConf.aiSeoHelper', 'array')
-        ->factory([service(ExtensionConfiguration::class), 'get'])
+        ->factory([new ReferenceConfigurator(ExtensionConfiguration::class), 'get'])
         ->args([
             'ai_seo_helper'
         ]);
 
     $services->set('CustomLanguageArray', 'array')
-        ->factory([service(CustomLanguageFactory::class), 'getCustomLanguages']);
+        ->factory([new ReferenceConfigurator(CustomLanguageFactory::class), 'getCustomLanguages']);
 
     $containerBuilder->register('Logger', LoggerInterface::class);
     $services->set('PsrLogInterface', 'Logger')
         ->factory([
-            service(LogManager::class), 'getLogger'
+            new ReferenceConfigurator(LogManager::class), 'getLogger'
         ]);
 
     $services->set(ContentService::class)
-        ->arg('$languages', service('CustomLanguageArray'))
-        ->arg('$extConf', service('ExtConf.aiSeoHelper'));
+        ->arg('$languages', new ReferenceConfigurator('CustomLanguageArray'))
+        ->arg('$extConf', new ReferenceConfigurator('ExtConf.aiSeoHelper'));
 
     $services->set(AiController::class)
-        ->arg('$contentService', service(ContentService::class))
-        ->arg('$logger', service('PsrLogInterface'))
+        ->arg('$contentService', new ReferenceConfigurator(ContentService::class))
+        ->arg('$logger', new ReferenceConfigurator('PsrLogInterface'))
         ->public();
 };
