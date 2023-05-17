ac1cf6271931e09d4163b61d7336e0237255acfa
Manuel Schnabel <mschnabel1992@gmail.com>
2023-05-17 15:28:18 +0200

    [TASK] Remove unused code block

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -5,13 +5,4 @@ return [
     'imports' => [
         '@passionweb/ai-seo-helper/' => 'EXT:ai_seo_helper/Resources/Public/JavaScript/',
     ],
-//    'imports' => [
-//        '@passionweb/ai-seo-helper/' => [
-//            'path' => 'EXT:ai_seo_helper/Resources/Public/JavaScript/',
-//            'exclude' => [
-//                'EXT:ai_seo_helper/Resources/Public/JavaScript/Helper/',
-//            ],
-//        ],
-//        'generateSuggestions' => 'EXT:ai_seo_helper/Resources/Public/JavaScript/Helper/generate-suggestions.js',
-//    ],
 ];
