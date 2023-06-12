34e4814b5b491cd1711aa8b8cf0ad45677e8492c
Manuel Schnabel <mschnabel1992@gmail.com>
2023-06-12 23:01:58 +0200

    [RELEASE] Update version to 0.5.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'PassionWeb Manuel Schnabel',
     'state' => 'beta',
     'clearCacheOnLoad' => true,
-    'version' => '0.5.2',
+    'version' => '0.5.3',
     'constraints' => [
         'depends' => ['typo3' => '10.4.0-12.4.99'],
         'conflicts' => [],
