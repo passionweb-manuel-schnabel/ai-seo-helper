f2215682fc0746701e32e58d0601f595db8e6f39
Manuel Schnabel <service@passionweb.de>
2024-02-12 20:50:42 +0100

    [TASK] Update supported models and some default values

--- Classes/Factory/SelectedModelFactory.php
+++ Classes/Factory/SelectedModelFactory.php
@@ -6,9 +6,20 @@ class SelectedModelFactory
 {
     public function checkSelectedModel($extConf): bool
     {
-        return  $extConf['openAiModel'] === 'gpt-3.5-turbo' ||
+        return  $extConf['openAiModel'] === 'gpt-3.5-turbo-16k-0613' ||
+                $extConf['openAiModel'] === 'gpt-3.5-turbo-0613' ||
                 $extConf['openAiModel'] === 'gpt-3.5-turbo-16k' ||
+                $extConf['openAiModel'] === 'gpt-3.5-turbo-instruct' ||
+                $extConf['openAiModel'] === 'gpt-3.5-turbo-1106' ||
+                $extConf['openAiModel'] === 'gpt-3.5-turbo' ||
+                $extConf['openAiModel'] === 'gpt-3.5-turbo-0125' ||
+                $extConf['openAiModel'] === 'gpt-4-32k-0613' ||
+                $extConf['openAiModel'] === 'gpt-4-32k' ||
+                $extConf['openAiModel'] === 'gpt-4-0613' ||
                 $extConf['openAiModel'] === 'gpt-4' ||
-                $extConf['openAiModel'] === 'gpt-4-32k';
+                $extConf['openAiModel'] === 'gpt-4-vision-preview' ||
+                $extConf['openAiModel'] === 'gpt-4-1106-preview' ||
+                $extConf['openAiModel'] === 'gpt-4-turbo-preview' ||
+                $extConf['openAiModel'] === 'gpt-4-0125-preview';
     }
 }
