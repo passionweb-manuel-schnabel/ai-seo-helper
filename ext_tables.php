1565261758034fd473151170cdf89f7fffe7acdc
SZChimp <47443911+SZChimp@users.noreply.github.com>
2024-06-14 15:53:18 +0200

    Update ext_tables.php
    Import dependencies via namespaces and use makeInstance() instead of native instantiation using `new`.

--- ext_tables.php
+++ ext_tables.php
@@ -1,10 +1,16 @@
 <?php
 
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 defined('TYPO3') or die();
 
-$pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
-if(new \TYPO3\CMS\Core\Information\Typo3Version() < 12 && empty($pageRenderer->getCharSet())) {
+$pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
+$typo3Version = GeneralUtility::makeInstance(Typo3Version::class);
+if ($typo3Version->getVersion() < 12 && empty($pageRenderer->getCharSet())) {
     $pageRenderer->setCharSet('utf-8');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_aiseohelper_domain_model_customlanguage');
+ExtensionManagementUtility::allowTableOnStandardPages('tx_aiseohelper_domain_model_customlanguage');
