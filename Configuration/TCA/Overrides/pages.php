d9045d9f68d72ee8c9dfaac69a93444a1923428f
Manuel Schnabel <service@passionweb.de>
2024-11-11 22:17:50 +0100

    [TASK] remove dependency check

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,91 +1,90 @@
 <?php
 
-if(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('ai_suite') === false) {
-    $GLOBALS['TCA']['pages']['columns']['description']['config'] = array_merge_recursive(
-        $GLOBALS['TCA']['pages']['columns']['description']['config'],
-        [
-            'fieldControl' => [
-                'importControl' => [
-                    'renderType' => 'aiSeoMetaDescription'
-                ]
+$GLOBALS['TCA']['pages']['columns']['description']['config'] = array_merge_recursive(
+    $GLOBALS['TCA']['pages']['columns']['description']['config'],
+    [
+        'fieldControl' => [
+            'importControl' => [
+                'renderType' => 'aiSeoMetaDescription'
             ]
         ]
-    );
+    ]
+);
 
-    $GLOBALS['TCA']['pages']['columns']['keywords']['config'] = array_merge_recursive(
-        $GLOBALS['TCA']['pages']['columns']['keywords']['config'],
-        [
-            'fieldControl' => [
-                'importControl' => [
-                    'renderType' => 'aiSeoKeywords'
-                ]
+$GLOBALS['TCA']['pages']['columns']['keywords']['config'] = array_merge_recursive(
+    $GLOBALS['TCA']['pages']['columns']['keywords']['config'],
+    [
+        'fieldControl' => [
+            'importControl' => [
+                'renderType' => 'aiSeoKeywords'
             ]
         ]
-    );
+    ]
+);
 
-    $GLOBALS['TCA']['pages']['columns']['seo_title']['config'] = array_merge_recursive(
-        $GLOBALS['TCA']['pages']['columns']['seo_title']['config'],
-        [
-            'fieldControl' => [
-                'importControl' => [
-                    'renderType' => 'aiSeoPageTitle'
-                ]
+$GLOBALS['TCA']['pages']['columns']['seo_title']['config'] = array_merge_recursive(
+    $GLOBALS['TCA']['pages']['columns']['seo_title']['config'],
+    [
+        'fieldControl' => [
+            'importControl' => [
+                'renderType' => 'aiSeoPageTitle'
             ]
         ]
-    );
+    ]
+);
 
-    $GLOBALS['TCA']['pages']['columns']['og_title']['config'] = array_merge_recursive(
-        $GLOBALS['TCA']['pages']['columns']['og_title']['config'],
-        [
-            'fieldControl' => [
-                'importControl' => [
-                    'renderType' => 'aiSeoOpenGraphTitle'
-                ]
+$GLOBALS['TCA']['pages']['columns']['og_title']['config'] = array_merge_recursive(
+    $GLOBALS['TCA']['pages']['columns']['og_title']['config'],
+    [
+        'fieldControl' => [
+            'importControl' => [
+                'renderType' => 'aiSeoOpenGraphTitle'
             ]
         ]
-    );
+    ]
+);
 
-    $GLOBALS['TCA']['pages']['columns']['twitter_title']['config'] = array_merge_recursive(
-        $GLOBALS['TCA']['pages']['columns']['twitter_title']['config'],
-        [
-            'fieldControl' => [
-                'importControl' => [
-                    'renderType' => 'aiSeoTwitterTitle'
-                ]
+$GLOBALS['TCA']['pages']['columns']['twitter_title']['config'] = array_merge_recursive(
+    $GLOBALS['TCA']['pages']['columns']['twitter_title']['config'],
+    [
+        'fieldControl' => [
+            'importControl' => [
+                'renderType' => 'aiSeoTwitterTitle'
             ]
         ]
-    );
+    ]
+);
 
-    $GLOBALS['TCA']['pages']['columns']['og_description']['config'] = array_merge_recursive(
-        $GLOBALS['TCA']['pages']['columns']['og_description']['config'],
-        [
-            'fieldControl' => [
-                'importControl' => [
-                    'renderType' => 'aiSeoOpenGraphDescription'
-                ]
+$GLOBALS['TCA']['pages']['columns']['og_description']['config'] = array_merge_recursive(
+    $GLOBALS['TCA']['pages']['columns']['og_description']['config'],
+    [
+        'fieldControl' => [
+            'importControl' => [
+                'renderType' => 'aiSeoOpenGraphDescription'
             ]
         ]
-    );
+    ]
+);
 
-    $GLOBALS['TCA']['pages']['columns']['twitter_description']['config'] = array_merge_recursive(
-        $GLOBALS['TCA']['pages']['columns']['twitter_description']['config'],
-        [
-            'fieldControl' => [
-                'importControl' => [
-                    'renderType' => 'aiSeoTwitterDescription'
-                ]
+$GLOBALS['TCA']['pages']['columns']['twitter_description']['config'] = array_merge_recursive(
+    $GLOBALS['TCA']['pages']['columns']['twitter_description']['config'],
+    [
+        'fieldControl' => [
+            'importControl' => [
+                'renderType' => 'aiSeoTwitterDescription'
             ]
         ]
-    );
+    ]
+);
 
-    $GLOBALS['TCA']['pages']['columns']['abstract']['config'] = array_merge_recursive(
-        $GLOBALS['TCA']['pages']['columns']['abstract']['config'],
-        [
-            'fieldControl' => [
-                'importControl' => [
-                    'renderType' => 'aiSeoAbstract'
-                ]
+$GLOBALS['TCA']['pages']['columns']['abstract']['config'] = array_merge_recursive(
+    $GLOBALS['TCA']['pages']['columns']['abstract']['config'],
+    [
+        'fieldControl' => [
+            'importControl' => [
+                'renderType' => 'aiSeoAbstract'
             ]
         ]
-    );
-}
+    ]
+);
+
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,72 +2,71 @@
 
 defined('TYPO3') or die('Access denied.');
 
-if(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('ai_suite') === false) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410677] = [
-        'nodeName' => 'aiSeoMetaDescription',
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410677] = [
+    'nodeName' => 'aiSeoMetaDescription',
+    'priority' => 30,
+    'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoMetaDescription::class
+];
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410678] = [
+    'nodeName' => 'aiSeoKeywords',
+    'priority' => 30,
+    'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoKeywords::class
+];
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410679] = [
+    'nodeName' => 'aiSeoPageTitle',
+    'priority' => 30,
+    'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoPageTitle::class
+];
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410680] = [
+    'nodeName' => 'aiSeoOpenGraphTitle',
+    'priority' => 30,
+    'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoOpenGraphTitle::class
+];
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410681] = [
+    'nodeName' => 'aiSeoTwitterTitle',
+    'priority' => 30,
+    'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoTwitterTitle::class
+];
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410682] = [
+    'nodeName' => 'aiSeoOpenGraphDescription',
+    'priority' => 30,
+    'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoOpenGraphDescription::class
+];
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410683] = [
+    'nodeName' => 'aiSeoTwitterDescription',
+    'priority' => 30,
+    'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoTwitterDescription::class
+];
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410689] = [
+    'nodeName' => 'aiSeoAbstract',
+    'priority' => 30,
+    'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoAbstract::class
+];
+
+if(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('news')) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410684] = [
+        'nodeName' => 'aiNewsMetaDescription',
         'priority' => 30,
-        'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoMetaDescription::class
+        'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\News\AiNewsMetaDescription::class
     ];
 
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410678] = [
-        'nodeName' => 'aiSeoKeywords',
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410685] = [
+        'nodeName' => 'aiNewsAlternativeTitle',
         'priority' => 30,
-        'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoKeywords::class
+        'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\News\AiNewsAlternativeTitle::class
     ];
 
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410679] = [
-        'nodeName' => 'aiSeoPageTitle',
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410686] = [
+        'nodeName' => 'aiNewsKeywords',
         'priority' => 30,
-        'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoPageTitle::class
+        'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\News\AiNewsKeywords::class
     ];
-
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410680] = [
-        'nodeName' => 'aiSeoOpenGraphTitle',
-        'priority' => 30,
-        'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoOpenGraphTitle::class
-    ];
-
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410681] = [
-        'nodeName' => 'aiSeoTwitterTitle',
-        'priority' => 30,
-        'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoTwitterTitle::class
-    ];
-
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410682] = [
-        'nodeName' => 'aiSeoOpenGraphDescription',
-        'priority' => 30,
-        'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoOpenGraphDescription::class
-    ];
-
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410683] = [
-        'nodeName' => 'aiSeoTwitterDescription',
-        'priority' => 30,
-        'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoTwitterDescription::class
-    ];
-
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410689] = [
-        'nodeName' => 'aiSeoAbstract',
-        'priority' => 30,
-        'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\AiSeoAbstract::class
-    ];
-
-    if(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('news')) {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410684] = [
-            'nodeName' => 'aiNewsMetaDescription',
-            'priority' => 30,
-            'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\News\AiNewsMetaDescription::class
-        ];
-
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410685] = [
-            'nodeName' => 'aiNewsAlternativeTitle',
-            'priority' => 30,
-            'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\News\AiNewsAlternativeTitle::class
-        ];
-
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1676410686] = [
-            'nodeName' => 'aiNewsKeywords',
-            'priority' => 30,
-            'class' => \Passionweb\AiSeoHelper\FormEngine\FieldControl\News\AiNewsKeywords::class
-        ];
-    }
 }
+
