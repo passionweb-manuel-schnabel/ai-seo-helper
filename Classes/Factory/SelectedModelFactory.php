0aeb324272631749132b0267ec6ea14017fb1816
Manuel Schnabel <service@passionweb.de>
2024-11-13 16:38:22 +0100

    [TASK] cleanup models

--- Classes/Factory/SelectedModelFactory.php
+++ Classes/Factory/SelectedModelFactory.php
@@ -6,17 +6,12 @@ class SelectedModelFactory
 {
     public function checkSelectedModel($extConf): bool
     {
-        return  $extConf['openAiModel'] === 'gpt-3.5-turbo-instruct' ||
-                $extConf['openAiModel'] === 'gpt-3.5-turbo-1106' ||
+        return  $extConf['openAiModel'] === 'gpt-3.5-turbo-1106' ||
                 $extConf['openAiModel'] === 'gpt-3.5-turbo' ||
                 $extConf['openAiModel'] === 'gpt-3.5-turbo-0125' ||
-                $extConf['openAiModel'] === 'gpt-4-32k-0613' ||
-                $extConf['openAiModel'] === 'gpt-4-32k' ||
-                $extConf['openAiModel'] === 'gpt-4-0613' ||
-                $extConf['openAiModel'] === 'gpt-4' ||
                 $extConf['openAiModel'] === 'gpt-4-1106-preview' ||
                 $extConf['openAiModel'] === 'gpt-4-turbo-preview' ||
-                $extConf['openAiModel'] === 'gpt-4-0125-preview' ||
+                $extConf['openAiModel'] === 'gpt-4-turbo' ||
                 $extConf['openAiModel'] === 'gpt-4o-mini';
     }
 }
